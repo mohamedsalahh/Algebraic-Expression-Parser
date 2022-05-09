335f11d70df3be407da0ecd74b0fafd3e8b495a1
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-05-09 03:18:59 +0200

    Delete AlgebraicExpressionParser directory

--- AlgebraicExpressionParser/__init__.py
+++
@@ -1,8 +0,0 @@
-from expression import Expression
-
-
-expression = Expression(expression="(1)*+00+0*", operators={'+', '&', '*'}, operators_info={'+': (2, 1), '&': (2, 2), '*': (1, 3)}, unwritten_operator='&')
-
-print(expression.postfix())
-print(expression.prefix())
-print(expression.tree())
