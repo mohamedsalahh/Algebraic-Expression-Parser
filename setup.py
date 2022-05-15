fee6cc46788b10ca76fc22d1b16ae174a07d7469
Mohamed Salah <61274371+mohamedsalahh@users.noreply.github.com>
2022-05-15 15:46:59 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""Setup logic for pip."""
+
+from setuptools import setup
+
+
+setup(
+    name='Algebraic-Expression-Parser',
+    version='0.0.1',
+    DESCRIPTION = 'Algebraic Expression Parser',
+    LONG_DESCRIPTION = 'Control and handle any algebraic expression, as well as do common expression operations such as getting postfix, prefix, and expression tree.',
+    url='https://github.com/mohamedsalahh/Algebraic-Expression-Parser',
+    author='Mohamed Salah',
+    author_email='mohamed.s636499@gmail.com',
+    license='MIT',
+    keywords='Algebraic Expression Parser',
+    install_requires=[
+          'binarytree',
+      ],
+)
